648dab63ec134202c380b4de4bd4465eda957c1c
Julian <jz.quark@gmail.com>
2019-07-05 13:50:17 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 try:
-	n = int(input("n:"))
+	n = int(input("n: "))
 	s = 0
 	for i in range(1, n):
 		if i % 3 == 0 or i % 5 == 0:
 			s += i
-	print(s)
+	print("sum: " + str(s))
 except:
 	print("Wrong format")	

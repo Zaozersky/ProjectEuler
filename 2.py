61cf81a1b1b4735c7360002df0e112bff793b895
Julian <jz.quark@gmail.com>
2019-07-05 14:07:10 +0300

    Add files via upload
    https://projecteuler.net/problem=2

---
+++ 2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+try:
+	n = int(input("n: "))
+	first = 1
+	second = 2
+	s = 0
+
+	while second <= n:
+		if second % 2 == 0:
+			s += second
+		second += first
+		first = second - first 
+	print("sum: " + str(s))
+except:
+	print("Wrong format")

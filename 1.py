b3e731ae56ba0cd5f0771f8173d71f896a492ab9
Julian <jz.quark@gmail.com>
2019-07-05 13:45:06 +0300

    Add files via upload
    https://projecteuler.net/problem=1

---
+++ 1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#import os
+
+#path = input("file path:")
+
+try:
+	n = int(input("n:"))
+	s = 0
+	for i in range(1, n):
+		if i % 3 == 0 or i % 5 == 0:
+			s += i
+	print(s)
+except:
+	print("Wrong format")	

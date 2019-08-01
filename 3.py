2126c6bc85521bbfadc1f6c12e33721d69f6ae8d
Julian <jz.quark@gmail.com>
2019-08-01 19:13:47 +0300

    Add files via upload
    https://projecteuler.net/problem=3

---
+++ 3.py
@@ -0,0 +1,33 @@
+import math
+
+primes = set()
+
+def isPrime(n):
+
+	if (n == 2):
+		return True
+
+	if (n in primes):
+		return True
+
+	m = int(math.sqrt(n))
+	ok = True
+	for i in range(2, m + 1):
+		if (n % i == 0):
+			ok = False
+			break
+
+	return ok
+
+
+n = int(input())
+
+m = int(math.sqrt(n))
+
+
+for i in range(2, m):
+	if (n % i == 0 and isPrime(i)):
+		primes.add(i)
+		primeFactor = i
+
+print(primeFactor)
